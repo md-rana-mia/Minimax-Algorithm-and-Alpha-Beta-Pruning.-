96664f1212a2253e22b9a220a29a8d3926ef5140
Md. Rana Mia <mdranamolla5555@gmail.com>
2026-08-21 22:32:32 +0600

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,41 @@
+# Minimax Algorithm and Alpha-Beta Pruning
 
+## Project Description
+This project implements the Minimax Algorithm and Alpha-Beta Pruning Algorithm using Python. The program evaluates a binary game tree and determines the optimal value.
+
+## Features
+- Implementation of Minimax Algorithm
+- Implementation of Alpha-Beta Pruning
+- Comparison of nodes evaluated by both algorithms
+- Counting of pruned nodes
+- Calculation of efficiency improvement
+
+## Algorithms Used
+1. Minimax Algorithm
+2. Alpha-Beta Pruning Algorithm
+
+## Test Cases
+### Case #1
+Generated Leaf Nodes:
+[3, 5, 2, 9, 12, 5, 23, 23]
+
+### Case #2
+Generated Leaf Nodes:
+[8, 6, 7, 4, 15, 10, 9, 11]
+
+## Programming Language
+Python
+
+## How to Run
+Run the following command:
+
+python main.py
+
+## Output
+The program displays:
+- Generated leaf node values
+- Minimax nodes evaluated
+- Optimal value
+- Alpha-Beta Pruning nodes evaluated
+- Number of pruned nodes
+- Efficiency improvement
